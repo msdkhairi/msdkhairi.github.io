f9d14031494588b58700876dfa7be3c54773282b
Masoud Khairi Atani <msdkhairi@gmail.com>
2023-06-14 23:06:35 -0700

    Add theme

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,8 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 import image from "@astrojs/image";
 
+
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://msdkhairi.github.io/',
