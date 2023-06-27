e6e3f91e4cc57ef619cd818508ff2e83e96a8057
Masoud Khairi Atani <msdkhairi@gmail.com>
2023-06-26 18:22:46 -0700

    Update site description

--- src/config.ts
+++ src/config.ts
@@ -2,6 +2,6 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'Homepage | Masoud Khairi Atani';
-export const SITE_DESCRIPTION = 'This is personal Website of Masoud Khairi Atani. My resume, contact info and contact info can be found here.';
+export const SITE_DESCRIPTION = 'This is personal Website of Masoud Khairi Atani. My resume and contact info can be found here.';
 export const LIGHT_THEME = "cmyk"
 export const DARK_THEME = "dark"
